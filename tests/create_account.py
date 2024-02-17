43d699b389bbe28c5f81c95b11eef2bcb6d7a5bd
darek <darecki9k@gmail.com>
2024-02-17 12:08:01 +0100

    first test in page object pattern - creating a new account / failed and passed

---
+++ tests/create_account.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+import random
+import pytest
+
+from pages.my_account_page import MyAccountPage
+
+
+@pytest.mark.usefixtures("setup")
+class TestCreateAccount:
+    def test_create_account_failed(self):
+        my_account_page = MyAccountPage(self.driver)
+        my_account_page.open_page()
+        my_account_page.create_account("6531testowanieselenium@testers.pl", "testowanieselenium")
+        msg = 'An account is already registered with your email address. Please log in.'
+        assert msg in my_account_page.get_error_msg()
+
+    def test_create_account_passed(self):
+        email = str(random.randint(0, 10000)) + "testowanieselenium@testers.pl"
+        my_account_page = MyAccountPage(self.driver)
+        my_account_page.open_page()
+        my_account_page.create_account(email, "testowanieselenium")
+
+        assert my_account_page.is_logout_link_displayed()
